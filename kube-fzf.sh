c2e2d1baa188252648d33e68580beb0e1be19486
Arunvel Sriram <arunvelsriram@gmail.com>
2018-03-15 23:19:07 +0530

    Use named arguments

--- kube-fzf.sh
+++ kube-fzf.sh
@@ -1,20 +1,57 @@
 #! /bin/bash
 
-findpod() {
-  local namespace=$1
-  local search_query=$2
+_kube_fzf_handler() {
+  local namespace pod_search_query
 
-  local kubectl_args=(--all-namespaces --no-headers)
+  while [ $# -gt 0 ]
+  do
+    local key="$1"
+    case $key in
+      -n)
+        namespace="$2"
+        shift
+        shift
+        ;;
+      -q)
+        pod_search_query="$2"
+        shift
+        shift
+        ;;
+      *)
+        echo "Unknown argument: $1"
+        echo "USAGE:"
+        echo "WIP"
+        return 1
+    esac
+  done
+
+  args="$namespace|$pod_search_query"
+  return 0
+}
+
+findpod() {
+  local namespace pod_search_query
+  _kube_fzf_handler "$@" || return 1
+  IFS=$'|' read -r namespace pod_search_query <<< "$args"
+
+  local namespace_arg="--all-namespaces"
   local pod_name_field=2
   if [ -n "$namespace" ]; then
-    kubectl_args[1]="--namespace=$namespace"
+    namespace_arg="--namespace=$namespace"
     pod_name_field=1
   fi
 
-  [ -n "$search_query" ] && local fzf_args="--query=$search_query"
+  [ -n "$pod_search_query" ] && local fzf_args="--query=$pod_search_query"
 
-  local pod_name=$(kubectl get pod $kubectl_args \
+  local pod_name=$(kubectl get pod $namespace_arg --no-headers \
     | awk -v field="$pod_name_field" '{ print $field }' \
     | fzf $fzf_args)
   echo $pod_name
+
+  _kube_fzf_cleanup
+  return 0
+}
+
+_kube_fzf_cleanup() {
+  unset args
 }
