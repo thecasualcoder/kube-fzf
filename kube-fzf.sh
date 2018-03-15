4da2c3bbdd64f5c4b9d16a7850698a353cc83df5
Arunvel Sriram <arunvelsriram@gmail.com>
2018-03-15 22:20:30 +0530

    Use local variables

--- kube-fzf.sh
+++ kube-fzf.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
 findpod() {
-  namespace=$1
-  search_query=$2
+  local namespace=$1
+  local search_query=$2
 
-  kubectl_args=(--all-namespaces --no-headers)
-  pod_name_field=2
+  local kubectl_args=(--all-namespaces --no-headers)
+  local pod_name_field=2
   if [ -n "$namespace" ]; then
     kubectl_args[1]="--namespace=$namespace"
     pod_name_field=1
   fi
 
-  pod_name=$(kubectl get pod $kubectl_args \
+  [ -n "$search_query" ] && local fzf_args="--query=$search_query"
+
+  local pod_name=$(kubectl get pod $kubectl_args \
     | awk -v field="$pod_name_field" '{ print $field }' \
-    | fzf --query $search_query)
+    | fzf $fzf_args)
   echo $pod_name
 }
-
